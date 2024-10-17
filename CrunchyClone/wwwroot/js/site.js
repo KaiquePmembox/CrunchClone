d867ccb3882e715ac30470830e4dd17ce9c940cc
kaique <kaiqueisilvapereira95@gmail.com>
2024-10-16 22:43:32 -0300

    pop up feature

--- CrunchyClone/wwwroot/js/site.js
+++ CrunchyClone/wwwroot/js/site.js
@@ -1,5 +1,8 @@
 ﻿$(document).ready(function () {
-    // Você pode adicionar animações ou efeitos aqui, se necessário
+    // Adiciona transição suave ao hover dos cartões de filmes
+    $('.movie-card').css('transition', 'transform 0.3s ease-in-out');
+
+    // Aplica o efeito de escala no hover
     $('.movie-card').hover(
         function () {
             $(this).css('transform', 'scale(1.1)');
